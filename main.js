0c5b90239689d8828488b334bf8e605d522fc4f6
Garieldas <dainiusrainys99@gmail.com>
2025-06-09 22:11:47 +0300

    Added checkWin function

--- main.js
+++ main.js
@@ -1,41 +1,77 @@
 const GameBoard = (function () {
-
-
-    const board = [["", "", ""],
+  const board = [
     ["", "", ""],
-    ["", "", ""]];
+    ["", "", ""],
+    ["", "", ""],
+  ];
 
-    const setMark = (row, column, symbol) => {
-        if (board[row][column] === ''){
-            board[row][column] = symbol;
-        }
+  const setMark = (row, column, symbol) => {
+    if (board[row][column] === "") {
+      board[row][column] = symbol;
     }
+  };
 
-    const getBoard = () => board;
+  const getBoard = () => board;
 
-    return { getBoard, setMark };
-
+  return { getBoard, setMark };
 })();
 
 const createPlayer = (name, score) => {
-    return {
-        name,
-        score
-    }
-}
+  return { name, score };
+};
 
 const GameController = (function () {
+  const checkWin = (board) => {
+    const size = board.length;
 
-    const winCondition = [
-        ['x', 'x', 'x'],
-        ['', '', ''],
-        ['', '', '']
-    ]
-    let playerChoice = prompt();
-    const filtered = playerChoice.split(',');
-    const [row, column, mark] = filtered;
-    GameBoard.setMark(row, column, mark);
+    for (let i = 0; i < size; i++) {
+      if (
+        board[i][0] !== "" &&
+        board[i].every((cell) => cell === board[i][0])
+      ) {
+        return board[i][0];
+      }
+    }
 
+    for (let j = 0; j < size; j++) {
+      let column = [];
+      for (let i = 0; i < size; i++) {
+        column.push(board[i][j]);
+      }
+      if (column[0] !== "" && column.every((cell) => cell === column[0])) {
+        return column[0];
+      }
+    }
+
+    let mainDiagonal = [];
+    for (let i = 0; i < size; i++) {
+      mainDiagonal.push(board[i][i]);
+    }
+    if (
+      mainDiagonal[0] !== "" &&
+      mainDiagonal.every((cell) => cell === mainDiagonal[0])
+    ) {
+      return mainDiagonal[0];
+    }
+
+    const rightDiagonal = [];
+    for (let i = 0; i < size; i++) {
+      rightDiagonal.push(board[i][size - i - 1]);
+    }
+    if (
+      rightDiagonal[0] !== "" &&
+      rightDiagonal.every((cell) => cell === rightDiagonal[0])
+    ) {
+      return rightDiagonal[0];
+    }
+
+    return "no Winner";
+  };
+
+  return { checkWin };
 })();
 
-console.log(GameBoard.getBoard())
+console.table(GameBoard.getBoard());
+
+const round = GameController.checkWin(GameBoard.getBoard());
+console.log("Winner:", round);
